eaa8dbdb6bf64f4ce8dd3ff063e37fb56107d5f4
Prina Tamang <121554323+prina1@users.noreply.github.com>
2024-01-11 20:52:46 +0545

    adding buttons

---
+++ react-first-project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+    <p>mybutton</p>
+    <header className="App-header">
+
+    </header>
+    </div>
+  );
+}
+
+export default App;
